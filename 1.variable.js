c2753e4552e6393792bf7eb90b6c049550607045
jadepark42 <hanafil9742@gmail.com>
2022-05-21 18:26:25 +0900

    const

--- 1.variable.js
+++ 1.variable.js
@@ -9,3 +9,35 @@ console.log(b);
 
 b =2;
 console.log(b);
+
+// var는 재선언과 재할당이 가능
+var name = 'jade'
+console.log(name)
+
+var name = 'patrick'
+console.log(name)
+
+name = 'hipster'
+console.log(name)
+
+// 이를 보완하기 위해 추가 된 변수 let과 const
+// let과 const는 재선언 불가
+// let은 재할당 가능
+let name = 'jade'
+console.log(name)
+
+let name = 'patrick'
+console.log(name)
+
+name = 'hipster'
+console.log(name)
+
+// const는 재할당 불가능
+const name = 'jade'
+console.log(name)
+
+const name = 'patrick'
+console.log(name)
+
+name = 'hipster'
+console.log(name)
